d095df5ce24497ce234d3cb3cfbc981bfe65063c
alli00152 <allison.alvarado2007@gmail.com>
2026-01-21 23:08:29 -0600

    Semana 1

---
+++ Tienda_AllisonAlvarado/src/main/java/Tienda_AllisonAlvarado/TiendaAllisonAlvaradoApplication.java
@@ -0,0 +1,13 @@
+package Tienda_AllisonAlvarado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAllisonAlvaradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAllisonAlvaradoApplication.class, args);
+	}
+
+}
---
+++ Tienda_AllisonAlvarado/src/test/java/Tienda_AllisonAlvarado/TiendaAllisonAlvaradoApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_AllisonAlvarado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAllisonAlvaradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
